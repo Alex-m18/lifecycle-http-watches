b5b02ed0bab2d778b114537a47bf2862b0d2e311
Alex-m18 <melnikov@tikhvin.org>
2020-06-06 21:40:37 +0300

    Updates

---
+++ precache-manifest.efdcd0666d0624e580c752294f68b87b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf44f70226c43d1bd44cd9091bf400a",
+    "url": "/ra16-lifecycle-http-watches/index.html"
+  },
+  {
+    "revision": "0856d4d82a6ff2d235a1",
+    "url": "/ra16-lifecycle-http-watches/static/css/main.e2775c4f.chunk.css"
+  },
+  {
+    "revision": "a79d45be32ded61b0eaa",
+    "url": "/ra16-lifecycle-http-watches/static/js/2.f9d82f25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ra16-lifecycle-http-watches/static/js/2.f9d82f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0856d4d82a6ff2d235a1",
+    "url": "/ra16-lifecycle-http-watches/static/js/main.fa0cc842.chunk.js"
+  },
+  {
+    "revision": "19946dc71b4755af2850",
+    "url": "/ra16-lifecycle-http-watches/static/js/runtime-main.527694c6.js"
+  }
+]);
