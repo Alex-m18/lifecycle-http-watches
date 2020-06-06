f63ee13fe7997ce2f74727b94d6cc732db1a4a56
Alex-m18 <melnikov@tikhvin.org>
2020-06-06 20:49:57 +0300

    Updates

---
+++ precache-manifest.ea66970c2b26e667d542be50f40f65f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bb17aa21a5d609018be3e9be50fa83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e9f758ceb77630361a",
+    "url": "/static/css/main.ee73e2eb.chunk.css"
+  },
+  {
+    "revision": "a79d45be32ded61b0eaa",
+    "url": "/static/js/2.f9d82f25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9d82f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e9f758ceb77630361a",
+    "url": "/static/js/main.2c9043ae.chunk.js"
+  },
+  {
+    "revision": "2cad6dad4278e7336438",
+    "url": "/static/js/runtime-main.25eddd2b.js"
+  }
+]);
